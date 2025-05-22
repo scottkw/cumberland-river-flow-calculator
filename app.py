ff5f9ed000d6a5e1e5eb197b34601e9ddf091da8
Ken Scott <kenscott@gmail.com>
2025-05-22 13:40:10 -0500

    style: adjust dam popup minimum width from 300px to 200px

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ if flow_cfs and flow_cfs > 0:
             ).add_to(m)
     # Calculate flow at user's location (nearest mile marker)
     cfm_at_user = int(flow_cfm_initial * ((1 - loss_rate) ** nearest_marker))
-    dam_popup_content = f"<pre style='white-space: pre; font-family: monospace; min-width: 300px; width: 400px;'>Old Hickory Dam<br>Lat: {nearest_lat:.5f}<br>Lon: {nearest_lon:.5f}</pre>"
+    dam_popup_content = f"<pre style='white-space: pre; font-family: monospace; min-width: 200px; width: 400px;'>Old Hickory Dam<br>Lat: {nearest_lat:.5f}<br>Lon: {nearest_lon:.5f}</pre>"
     folium.CircleMarker(
         location=[nearest_lat, nearest_lon],
         radius=8,
